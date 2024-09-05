cb9648bb77fe2b38e7595a508f66350a74f78f76
Delainy Silva <delainysilvadias@gmail.com>
2024-09-05 16:00:53 -0300

    Changing the project styling and adding JavaScript pages

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "UI/UX Design",
+        descricao: "Crie experiências digitais incríveis! Um(a) UI/UX Designer cria interfaces intuitivas e visuais, garantindo que os usuários naveguem com facilidade e satisfação. Utilize ferramentas como Figma, Adobe XD e Sketch para dar vida a suas ideias.",
+        link: "https://www.alura.com.br/cursos-online-design-ux?srsltid=AfmBOor4Cbxb1LlPA9OHIxYyniNfGaHCfxV2jfDuSKQUt2Dp0cA_nZhw"
+    },
+    {
+        titulo: "Desenvolvedor Front-end",
+        descricao: "Construa a parte visual das aplicações web! Um desenvolvedor front-end utiliza linguagens como HTML, CSS e JavaScript para criar interfaces interativas e dinâmicas.",
+        link: "https://www.alura.com.br/cursos-online-frontend"
+    },
+    {
+        titulo: "Desenvolvedor Back-end",
+        descricao: "Crie a lógica por trás das aplicações! Um desenvolvedor back-end trabalha com linguagens como Python, Java e Node.js para construir a parte do servidor de uma aplicação.",
+        link: "https://www.alura.com.br/cursos-online-backend"
+    },
+    {
+        titulo: "Cientista de Dados",
+        descricao: "Extraia insights valiosos a partir de dados! Um cientista de dados utiliza técnicas estatísticas e machine learning para analisar grandes volumes de dados e tomar decisões estratégicas.",
+        link: "https://www.alura.com.br/cursos-online-data-science"
+    },
+    {
+        titulo: "Engenheiro de Software",
+        descricao: "Desenvolve e mantém sistemas de software! Um engenheiro de software aplica princípios de engenharia para projetar, implementar e testar software.",
+        link: "https://www.alura.com.br/cursos-online-engenharia-de-software"
+    },
+    {
+        titulo: "DevOps",
+        descricao: "Conecta desenvolvimento e operações! Um profissional DevOps automatiza processos e garante a entrega contínua de software.",
+        link: "https://www.alura.com.br/cursos-online-devops"
+    }
+];
