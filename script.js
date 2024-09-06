ab5a4d084a1c4f05bc3b51eec9b1d726663321af
Delainy Silva <delainysilvadias@gmail.com>
2024-09-05 21:30:50 -0300

    Making the page dynamic with JavaScript, using for, let, document.getElementId

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log(dados);
+function pesquisar(){
+    console.log("clicou")
+}
+
+
+
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+
+let resultados = ""
+
+for(let dado of dados){
+    resultados += `
+    <div class="card-carees">
+       <h3>${dado.titulo}</h3>
+         <div class="line"></div>
+         <p>${dado.descricao}
+        </p>
+            <button><a href=${dado.link} target="_blank">Mais Informações</a></button>
+    </div>
+`
+}
+
+section.innerHTML = resultados;
