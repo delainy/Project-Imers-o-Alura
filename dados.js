ee86d8ffc1a0d381edf40f69d5098db9196ca909
Delainy Silva <delainysilvadias@gmail.com>
2024-09-06 21:02:51 -0300

    Project Finished

--- dados.js
+++ dados.js
@@ -2,31 +2,61 @@ let dados = [
     {
         titulo: "UI/UX Design",
         descricao: "Crie experiências digitais incríveis! Um(a) UI/UX Designer cria interfaces intuitivas e visuais, garantindo que os usuários naveguem com facilidade e satisfação. Utilize ferramentas como Figma, Adobe XD e Sketch para dar vida a suas ideias.",
-        link: "https://www.alura.com.br/cursos-online-design-ux?srsltid=AfmBOor4Cbxb1LlPA9OHIxYyniNfGaHCfxV2jfDuSKQUt2Dp0cA_nZhw"
+        link: "https://www.alura.com.br/cursos-online-design-ux?srsltid=AfmBOor4Cbxb1LlPA9OHIxYyniNfGaHCfxV2jfDuSKQUt2Dp0cA_nZhw",
+        tecnologias: "Figma, Adobe XD, Sketch, InVision, Framer"
     },
     {
         titulo: "Desenvolvedor Front-end",
         descricao: "Construa a parte visual das aplicações web! Um desenvolvedor front-end utiliza linguagens como HTML, CSS e JavaScript para criar interfaces interativas e dinâmicas.",
-        link: "https://www.alura.com.br/cursos-online-frontend"
+        link: "https://www.alura.com.br/escola-front-end",
+        tecnologias: "Html, Css, JavaScript, React, Angular, Vue.js"
     },
     {
         titulo: "Desenvolvedor Back-end",
         descricao: "Crie a lógica por trás das aplicações! Um desenvolvedor back-end trabalha com linguagens como Python, Java e Node.js para construir a parte do servidor de uma aplicação.",
-        link: "https://www.alura.com.br/cursos-online-backend"
+        link: "https://www.alura.com.br/escola-programacao",
+        tecnologias: "Python, Java, JavaScript, PHP, Ruby, SQL,"
     },
     {
         titulo: "Cientista de Dados",
         descricao: "Extraia insights valiosos a partir de dados! Um cientista de dados utiliza técnicas estatísticas e machine learning para analisar grandes volumes de dados e tomar decisões estratégicas.",
-        link: "https://www.alura.com.br/cursos-online-data-science"
+        link: "https://www.alura.com.br/escola-data-science",
+        tecnologias: "Python, R, SQL, Jupyter, Power BI"
     },
     {
         titulo: "Engenheiro de Software",
         descricao: "Desenvolve e mantém sistemas de software! Um engenheiro de software aplica princípios de engenharia para projetar, implementar e testar software.",
-        link: "https://www.alura.com.br/cursos-online-engenharia-de-software"
+        link: "https://www.alura.com.br/escola-programacao",
+        tecnologias: "C/C++, Java, C#, Git"
     },
     {
         titulo: "DevOps",
         descricao: "Conecta desenvolvimento e operações! Um profissional DevOps automatiza processos e garante a entrega contínua de software.",
-        link: "https://www.alura.com.br/cursos-online-devops"
-    }
+        link: "https://www.alura.com.br/escola-devops",
+        tecnologias: "Bash, Python, Docker, Ansible"
+    },
+    {
+        titulo: "Desenvolvedor de Jogos",
+        descricao: "Crie universos virtuais! Um desenvolvedor de jogos utiliza engines como Unity e Unreal Engine para criar experiências de jogo imersivas.",
+        link: "https://www.alura.com.br/escola-programacao",
+        tecnologias: "C#, C++, Unity, Unreal Engine, JavaScript, Python"
+      },
+      {
+        titulo: "Especialista em Cibersegurança",
+        descricao: "Proteja sistemas e dados! Um especialista em cibersegurança utiliza ferramentas e técnicas para identificar e mitigar ameaças à segurança da informação.",
+        link: "https://www.alura.com.br/escola-programacao",
+        tecnologias: "Python, Kali Linux, Nmap, Metasploit, OWASP ZAP"
+      },
+      {
+        titulo: "Analista de Dados",
+        descricao: "Transforme dados em conhecimento! Um analista de dados coleta, organiza e analisa dados para gerar insights e tomar decisões estratégicas.",
+        link: "https://www.alura.com.br/escola-programacao",
+        tecnologias: "SQL, Python, R, Power BI, Tableau"
+      },
+      {
+        titulo: "Hacker Ético",
+        descricao: "Teste a segurança de sistemas! Um hacker ético utiliza suas habilidades para identificar vulnerabilidades em sistemas e ajudar a protegê-los.",
+        link: "https://www.alura.com.br/escola-programacao",
+        tecnologias: "Python, Kali Linux, Burp Suite, Metasploit"
+      }
 ];
--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-JavaScript
+
 function pesquisar() {
   // Seleciona a seção onde os resultados da pesquisa serão exibidos pelo seu ID.
   let section = document.getElementById("resultados-pesquisa");
 
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // Transforma todo o texto do campo de pesquisa em letra minúscula
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return;
+    }
+
   // Inicializa uma string vazia para armazenar os resultados formatados em HTML.
   let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tecnologia = "";
 
   // Itera sobre cada elemento (dado) no array 'dados'.
   for (let dado of dados) {
-    // Concatena uma nova div com as informações do dado atual à string 'resultados'.
-    // As informações são formatadas como HTML para criar uma card com título, descrição e botão.
-    resultados += `
-      <div class="card-carees">
-        <h3>${dado.titulo}</h3> <div class="line">
-        </div> <p>${dado.descricao}</p> 
-        <button><a href=${dado.link} target="_blank">Mais Informações</a></button> 
+
+    titulo = dado.titulo.toLowerCase();
+    descricao = dado.descricao.toLowerCase();
+    tecnologia = dado.tecnologias.toLowerCase();
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tecnologia.includes(campoPesquisa)){
+        //cria um novo elemento
+        resultados += `
+        <div class="card-carees">
+            <h3>${dado.titulo}</h3> 
+            <div class="line"></div> 
+            <p>${dado.descricao}</p> 
+            <div class="line"></div> 
+            <h4>Principais Tecnologias</h4>
+            <span class="tecnologia-card" >${dado.tecnologias}</span>
+            <button><a href=${dado.link} target="_blank">Mais Informações</a></button> 
         </div>
     `;
+    }
+  }
+
+  if(!resultados){
+    resultados = "<p>Nada foi encontrado</p>"
   }
 
   // Atribui o conteúdo da string 'resultados' (agora com todas as cards) ao HTML da seção.
